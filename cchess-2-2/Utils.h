0c4a287e8c526d4aa246ae9a653836f8416484c8
Henry Wilder <henrythepony@gmail.com>
2023-07-04 10:02:03 -0400

    Worked on utils

--- cchess-2-2/Utils.h
+++ cchess-2-2/Utils.h
@@ -4,6 +4,8 @@
 #include <thread>
 #include <concepts>
 
+template<typename T> concept numeric = std::is_integral_v<T> || std::is_floating_point_v<T>;
+
 // Get a console handle
 extern HWND window;
 
@@ -47,29 +49,36 @@ bool Even(int a);
 struct Coord;
 struct PixelPos;
 
+// A generic 2D integer.
+// Can be added/subtracted.
 struct Vec2
 {
     template<std::floating_point fT>
     Vec2(fT x, fT y) :
         x{ (int)(x + ((fT)0.5)) },
         y{ (int)(y + ((fT)0.5)) }
-    {};
+    {}
 
     template<std::integral iT>
     Vec2(iT x, iT y) :
         x{ (int)x },
         y{ (int)y }
-    {};
+    {}
 
     Vec2(int x, int y) :
         x{ x },
         y{ y }
-    {};
+    {}
+
+    Vec2(const Vec2& in) :
+        x{ in.x },
+        y{ in.y }
+    {}
 
     Vec2() :
         x{ 0 },
         y{ 0 }
-    {};
+    {}
 
     int x, y;
 
@@ -99,16 +108,28 @@ struct Vec2
 
 bool ValidPos(const Coord testPos);
 
-// Implies (to programmers) screenspace position
-// Can be multiplied/divided
+// Screenspace position.
+// Can be multiplied/divided and scaled.
 struct PixelPos : public Vec2
 {
-    template<std::floating_point fT> PixelPos(fT x, fT y) : Vec2(x,y) {};
-    template<std::integral iT> PixelPos(iT x, iT y) : Vec2(x,y) {};
-    PixelPos(int x, int y) : Vec2(x,y) {};
-    PixelPos(Vec2 in) : Vec2( in ) {};
-    PixelPos() : Vec2(0,0) {};
+    template<numeric nT>
+    PixelPos(nT x, nT y) :
+        Vec2(x,y)
+    {}
 
+    PixelPos(const Vec2& in) :
+        Vec2(in)
+    {}
+
+    PixelPos(const PixelPos& in) :
+        Vec2(in.x, in.y)
+    {}
+
+    PixelPos() :
+        Vec2()
+    {}
+
+    // Conversion to boardspace.
     operator Coord() const;
 
     PixelPos& operator=(const PixelPos b)
@@ -202,31 +223,51 @@ struct PixelPos : public Vec2
 PixelPos Snap(PixelPos pix, PixelPos gridsize);
 PixelPos Snap(PixelPos pix, int gridsize);
 
-// Implies (to programmers) boardspace position
-// Can be tested for equivalence
+// Boardspace position.
+// Can be compared for equality (but not greater/less than).
 struct Coord : public Vec2
 {
-    Coord() : Vec2({ 0,0 }) {};
-    Coord(Vec2 in) : Vec2( in ) {};
-    Coord(int _x, int _y) : Vec2({ _x,_y }) {};
+    template<numeric nT>
+    Coord(nT x, nT y) :
+        Vec2(x,y)
+    {}
 
+    Coord(const Vec2& in) :
+        Vec2(in)
+    {}
+
+    Coord(const Coord& in) :
+        Vec2(in.x, in.y)
+    {}
+
+    Coord() :
+        Vec2()
+    {}
+
+    // Conversion to screenspace.
     operator PixelPos() const;
 
-    bool operator==(const Coord& p2) const {
-        return (x == p2.x) && (y == p2.y);
+    bool operator==(const Coord& p2) const
+    {
+        return
+            (x == p2.x) &&
+            (y == p2.y);
     }
-    bool operator!=(const Coord& p2) const {
-        return (x != p2.x) || (y != p2.y);
+    bool operator!=(const Coord& p2) const
+    {
+        return
+            (x != p2.x) ||
+            (y != p2.y);
     }
 };
 
-// Takes the coordinates of a board tile and returns the top-left corner pixel of that space
+// Returns the top-left pixel of the input board space.
 PixelPos PixelSpace(const Coord tile);
 
-// Takes the position of a pixel and returns the tile that pixel resides within
+// Returns the coordinate of the board tile containing the pixel.
 Coord BoardSpace(const PixelPos pixel);
 
-// The start and end positions of a space
+// The start and end positions of a space.
 struct BoardTile
 {
     BoardTile();
@@ -235,6 +276,8 @@ struct BoardTile
     PixelPos start, end;
 };
 
+// I think the idea of this was so that the pixels could be iterated over using a ranged for-loop.
+// This can be performed better by simply using begin() and end() methods.
 #if false
 // An array of all pixels in a board tile
 struct BoardTile2
